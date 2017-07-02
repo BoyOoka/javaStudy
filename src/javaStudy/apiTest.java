75cdbaf604857027e13b1264bf797d87791e2fa1
gaya <gaya@192.168.0.102>
2017-07-02 16:44:14 +0800

    增加java doc

---
+++ src/javaStudy/apiTest.java
@@ -0,0 +1,19 @@
+package javaStudy;
+
+public class apiTest {
+
+	public static void main(String[] args) {
+		
+		long time = System.currentTimeMillis();
+		int max = Math.max(3, 5);
+		double pi = Math.PI;
+		double abs = Math.abs(-32.1);//绝对值
+		double hypot = Math.hypot(3, 4);
+		System.out.println(time+" "+max);
+		System.out.println(pi);
+		System.out.println(abs);
+		System.out.println(hypot);
+		
+	}
+
+}
